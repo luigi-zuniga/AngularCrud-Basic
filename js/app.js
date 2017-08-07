5827c6c990f0c517ab23e44f55550ae522b3cdd2
Luis <Luis@SCE_LEICA2.inacap.cl>
2017-08-07 12:57:37 -0400

    C3 Add AngularJS and ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-var myApp  =  angular.module( 'crudController', []);
+var myApp = angular.module('crudController', []);
 
-myApp = controller('crudController',['$scope' , function($scope){
-   
+myApp.controller('crudController', ['$scope', function ($scope) {
+
+    console.log('In my Array Controller');
+
     $scope.users = [
-        {username:"Francisca",email:"fran.lore@gmai.com",fullname:"Lorena Gonzalez"}
-    ]
-    
+        {
+            username: "Luigi",
+            email: "luigi.zun@gmai.com",
+            fullname: "Albert Zuniga"
+        },
+        {
+            username: "Jhon",
+            email: "Jhon.Salc@gmai.com",
+            fullname: "Jhon Salc"
+        },
+        {
+            username: "Hector",
+            email: "Hector.per@gmai.com",
+            fullname: "Soto Perez"
+        },
+        {
+            username: "diego",
+            email: "liego.zaf@gmai.com",
+            fullname: "Zafrada maulme"
+        }
+    ];
+
 }])
